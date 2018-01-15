408a0672d14ce3cee176e731b560804fc36ba098
hyunki95 <dmsrlgusrl@naver.com>
2018-01-15 15:20:47 +0900

    한경빅데이터과정
    총 모음

---
+++ Hs.py
@@ -0,0 +1,13 @@
+def hot_sum(a,b,c):
+    hs=(a+b+c)
+    return hs
+
+def hot_mi(a,b,c):
+    hm=(a*b*c)
+    return hm
+
+def hot_aver(a,b,c):
+    ha= (a+b+c)/3
+    return ha
+    
+    
---
+++ ex6.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+읽기=pd.read_csv('./NHIS_OPEN_T20_2008.csv',engine="python")
+읽기
+
